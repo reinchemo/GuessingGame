df41895b93e1c67b34c4edcdd441e1bff5e51cfe
Reinhard kiprop <chemorein24@gmail.com>
2023-05-25 14:27:59 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,93 @@
+//* DOM selection //
+var gameBoard = {
+  displayBox: document.querySelector(".display-box"),
+  life: document.querySelector("#life"),
+  point: document.querySelector("#point"),
+  pointAnim: document.querySelector("#point-anim"),
+  result: document.querySelector(".result"),
+  keypadDisplay: document.querySelector(".numpad-display"),
+  numpadBox: document.querySelector(".numpad-box"),
+  keypad1: document.querySelector("#keypad1"),
+  keypad2: document.querySelector("#keypad2"),
+  keypad3: document.querySelector("#keypad3"),
+  keypad4: document.querySelector("#keypad4"),
+  keypad5: document.querySelector("#keypad5"),
+  keypadEnter: document.querySelector("#enter")
+}
+
+//* Gameborad life create //
+var lifeArray = [];
+for (var i = 0; i < 5; i++) {
+  lifeArray.push("<i class=\"fas fa-heart\"></i>");
+}
+
+gameBoard.life.innerHTML = lifeArray; //* printing gameBoard life //
+
+var inputHistory = []; //* total guess history //
+var pointArray = [0]; //* gameBoard point array //
+
+var random = Math.ceil(Math.random() * 5); //* genarate a random number within 1-5
+
+//! The keypad funtion --> defined the number what user clicked //
+function keypad(key) {
+  gameBoard.keypadDisplay.innerHTML = key; //* display user selected number
+  inputHistory.push(key) //* pushing selected number in inputHistory
+}
+
+//! GAME BOARD MAIN FUNCTION //
+function Enter() {
+
+  //* if condition is true player earn 5 points
+  if (inputHistory[inputHistory.length - 1] == random) {
+    pointArray[0] += 5;
+    random = Math.ceil(Math.random() * 5); //* genarate a random number within 1-5
+
+    gameBoard.pointAnim.innerHTML = "<span>+5</span>"
+    gameBoard.result.innerHTML = "Right Guess"; //* print result box message
+
+    //! Win Condition --> if player point reached 30 player will be win
+    if (pointArray[0] >= 30) {
+      gameBoard.result.innerHTML = "<span id=\"game-win\">Congratulation<br>You Win!</span><br><a href=\"\"><i class=\"fas fa-sync-alt\"></i></a>"
+      gameBoard.numpadBox.innerHTML += "<div class=\"button-disable\"></div>";
+    } else {
+      //* Gameborad life reset  //
+      if (lifeArray.length < 5) {
+        lifeArray = []; //* re-assign array in lifeArray
+        for (var x = 0; x < 5; x++) {
+          lifeArray.push("<i class=\"fas fa-heart\"></i>");
+        }
+      }
+    }
+
+    //! Low life Alert
+    if (lifeArray.length < 3) {
+      gameBoard.displayBox.style.animationName = "anime";
+    } else {
+      gameBoard.displayBox.style.animationName = "";
+    }
+
+    gameBoard.point.innerHTML = pointArray[0]; //* print point
+    gameBoard.life.innerHTML = lifeArray; //* print life
+
+  } else {
+    lifeArray.pop(); //* poping array for reducing player life
+
+    //! Low life Alert
+    if (lifeArray.length < 3) {
+      gameBoard.displayBox.style.animationName = "anime";
+    } else {
+      gameBoard.displayBox.style.animationName = "";
+    }
+
+
+    gameBoard.life.innerHTML = lifeArray; //* print life
+    gameBoard.result.innerHTML = random > inputHistory[inputHistory.length - 1] ? "Too Low!" : "Too High!" //* print guess nummber
+
+    //! GAME OVER CONDITION
+
+    if (lifeArray.length < 1) {
+      gameBoard.result.innerHTML += "<br><span id=\"game-over\">Game Over</span><br><a href=\"\"><i class=\"fas fa-sync-alt\"></i></a>"
+      gameBoard.numpadBox.innerHTML += "<div class=\"button-disable\"></div>";
+    }
+  }
+}
